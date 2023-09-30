61bda7caa551d22788f5b74a56351043828e4bed
ef vl <efvl2022@gmail.com>
2023-09-30 09:52:16 +0200

    add page controller

---
+++ src/main/java/dev/evv/aservice/controller/PageController.java
@@ -0,0 +1,25 @@
+package dev.evv.aservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/v1/page")
+public class PageController {
+
+    @GetMapping("/test")
+    public String testString(){
+        String curIP = "";
+        try {
+            curIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        return "AService test string " + curIP;
+    }
+
+}
